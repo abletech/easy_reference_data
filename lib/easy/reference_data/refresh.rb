b204beaa7aeb57e86895132bfb919dd636742a95
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2012-11-15 16:01:00 +1300

    don't use mass assignment operation for creating new records

--- lib/easy/reference_data/refresh.rb
+++ lib/easy/reference_data/refresh.rb
@@ -4,7 +4,8 @@ module Easy
       record = clazz.where(unique_attribute_symbol => unique_attribute_value).first
 
       if record.nil?
-        record = clazz.new(unique_attribute_symbol => unique_attribute_value)
+        record = clazz.new
+        record.send "#{unique_attribute_symbol}=", unique_attribute_value
       end
 
       attributes.each_pair do |key, value|
@@ -12,9 +13,9 @@ module Easy
       end
 
       if record.new_record?
-        puts "  creating #{clazz}(#{unique_attribute_value})"
+        puts "..creating #{clazz}(#{unique_attribute_value})"
       elsif record.changed?
-        puts "  updating #{clazz}(#{unique_attribute_value})"
+        puts "..updating #{clazz}(#{unique_attribute_value})"
       end
 
       begin
